7059909b6dc01d4f166b64c8e9975d9d1f025fdf
lydialei0710 <lydialei0710@gmail.com>
2024-06-28 01:32:50 -0400

    finance page entirety!!

---
+++ Prododoro/ProgressBars.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressBars.swift
+//  Prododoro
+//
+//  Created by Lydia Lei on 6/28/24.
+//
+
+import SwiftUI
+
+struct ProgressBars: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressBars()
+}
